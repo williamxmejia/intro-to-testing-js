e2562d37816b6eafd16849eb55dbc84b048b028f
William Mejia <willx1995@gmail.com>
2022-02-07 10:13:27 -0600

    add tests and functions

--- code.js
+++ code.js
@@ -53,4 +53,6 @@ function isVowel(vow){
     return typeof vow === 'string' && vow.toLowerCase() === "a";
 }
 
-
+function add(num1, num2){
+    return Number(num1) + Number(num2)
+}
--- tests.js
+++ tests.js
@@ -114,4 +114,31 @@ describe('isVowel', function(){
     it('should return "false" when executed', function(){
         expect(isVowel(true || false)).toBe(false);
     });
+});
+
+describe('add', function(){
+    it('should expect a function', function(){
+        expect(typeof add).toBe('function');
+    });
+    it('should return "5" when executed', function(){
+        expect(add(2, 3)).toBe(5);
+    });
+    it('should return "-12" when executed', function(){
+        expect(add(-3, -9)).toBe(-12);
+    });
+    it('should return "11" when executed', function(){
+        expect(add("5", 6)).toBe(11);
+    });
+    it('should return "6" when executed', function(){
+        expect(add("-4","10")).toBe(6);
+    });
+    it('should return "NaN" when executed', function(){
+        expect(add('banana', 'split')).toBeNaN();
+    });
+    it('should return "NaN" when executed', function(){
+        expect(add(2, 'apples')).toBeNaN();
+    });
+    it('should return "NaN" when executed', function(){
+        expect(add()).toBeNaN();
+    });
 })
